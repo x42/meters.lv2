ea2343f0e8eb6b827e2dd744350d1782f1324e23
Robin Gareus <robin@gareus.org>
2014-07-12 23:37:44 +0200

    fix fft static builds on 64bit platforms

--- static_fft.sh
+++ static_fft.sh
@@ -2,5 +2,5 @@
 set -e
 curl -L http://www.fftw.org/fftw-3.3.4.tar.gz | tar xz 
 cd fftw-3.3.4/
-CFLAGS=-fvisibility=hidden ./configure --enable-single --enable-sse --enable-avx --disable-mpi --disable-shared --enable-static
+CFLAGS="-fvisibility=hidden -fPIC" ./configure --enable-single --enable-sse --enable-avx --disable-mpi --disable-shared --enable-static
 make
