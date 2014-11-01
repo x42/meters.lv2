558259f2e80d6153be710f41e0bb86c02ac58943
Robin Gareus <robin@gareus.org>
2014-11-01 03:45:04 +0100

    update static fftw compile script for good measure
    no -fvisibility=internal but exclude,ALL

--- static_fft.sh
+++ static_fft.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
-curl -L http://www.fftw.org/fftw-3.3.4.tar.gz | tar xz 
+curl -L http://www.fftw.org/fftw-3.3.4.tar.gz | tar xz
 cd fftw-3.3.4/
-CFLAGS="-fvisibility=hidden -fPIC" ./configure --enable-single --enable-sse --enable-avx --disable-mpi --disable-shared --enable-static
-make
+CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
+	./configure \
+	--enable-single --enable-sse --enable-avx --disable-mpi \
+	--disable-shared --enable-static
+make -j2
